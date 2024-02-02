79a6fb8bd173a113576197d2d4786e2951bd6d18
DEVAVISWA G <devaviswa15@gmail.com>
2024-02-02 15:57:13 +0530

    Error commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Home from './components/Home/Home'
+import Project from './components/Project/Project'
+import Contact from './components/Contact/Contact'
+import '../src/App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Home />
+      <About />
+      <Project />
+      <Contact /> */}
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/project' element={<Project />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='component'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aspernatur voluptatum fugiat quisquam sint natus a inventore amet omnis, libero accusamus, molestiae corrupti temporibus, dicta expedita enim ad iste illum?
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae facilis vero voluptatibus animi minima eveniet voluptatem architecto qui dolorem voluptatum? Ut modi accusamus consequatur atque fugit dolor laborum? Dolorem, totam!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolore quo voluptatum aliquid inventore expedita quidem illo ratione, veritatis molestiae vel obcaecati provident ad maiores, eaque recusandae ullam, aliquam unde.
+    </div>
+  )
+}
+
+export default About
